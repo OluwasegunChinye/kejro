80bd78ffd919dab6af1bda81729cb15b30e62dc9
oluwasegunchinye <oluwasegunchinye@gmail.com>
2023-05-20 12:23:14 +0100

    init

--- src/navigation/NavContainer.js
+++ src/navigation/NavContainer.js
@@ -5,13 +5,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import GetStarted from '../screens/GetStarted';
 import SignUp from '../screens/SignUp';
 import { Register } from '../screens';
+import BottomTabs from './BottomTabs';
 
 const Stack = createNativeStackNavigator();
 
 const NavContainer = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShadowVisible: false, headerShown: false }}>
+            <Stack.Navigator screenOptions={{ headerShadowVisible: false, headerShown: false }} initialRouteName=''>
                 <Stack.Screen name="Get started" component={GetStarted}  />
                 {/* <Stack.Screen name="bottomtabs" component={BottomTabs}  options={{headerShown: false}}/> */}
                 <Stack.Screen  name='signup' component={SignUp}/>
--- src/screens/More.js
+++ src/screens/More.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 const More = () => {
   return (
-    <View>
-      <Text>More</Text>
+    <View style={{backgroundColor: 'red'}}>
+      <Text>More Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus ullam unde ducimus perferendis error? Praesentium voluptatem nihil mollitia quisquam quia quasi, accusantium, eligendi, adipisci quis distinctio nemo magni delectus! Et, in animi. Eos laudantium eveniet dicta nam pariatur, quo quae aliquam odio repudiandae! Maxime fugit, harum sed asperiores explicabo, impedit illum eveniet reprehenderit suscipit minima sunt doloremque architecto vel praesentium. Maiores, tempora? Vero ipsam repellendus officia voluptate id. Neque adipisci facere reiciendis ipsam minima maiores quibusdam quisquam amet consectetur, sequi, debitis eveniet dicta voluptatibus nihil, fugit ducimus quia at? Voluptates qui sed fugit assumenda ab praesentium amet, corporis eligendi minus.</Text>
     </View>
   )
 }
