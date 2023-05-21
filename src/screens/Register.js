21c917bced71d79acebcb4bee338e257bd30fce5
oluwasegunchinye <oluwasegunchinye@gmail.com>
2023-05-21 19:02:48 +0100

    init test

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -3,12 +3,9 @@ import React from 'react';
 
 const Register = ({ route, navigation }) => {
     const user = route.params.userName;
-    
+
     return (
         <SafeAreaView className="items-center">
-            <View className="mt-10">
-                <Text className="text-green-900">Register</Text>
-            </View>
             <View>
                 <Text>Hello {user},</Text>
             </View>
